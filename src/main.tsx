4ca766a0217fbcc5e0a6122e5f4b231b8371233d
appleseed0910 <xinyi.hu01@sap.com>
2026-03-30 16:49:44 +0800

    init commit, add favicon, title, remove default content

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+
+// // 初始化
+// document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px'; 
+// // 以 375 为标准比例，等比放大缩小 1rem对应的大小，1rem即html的fontSize
+
+// // 放大缩小的时候刷一下
+// window.addEventListener('resize', () => {
+//   document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px'; 
+// })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
