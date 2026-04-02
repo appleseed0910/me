aee488380c5abb48bb34f586edac68a4e321775a
appleseed0910 <appleseed0910@gmail.com>
2026-04-02 18:36:31 +0800

    done content title template

--- src/components/Content/About/About.tsx
+++ src/components/Content/About/About.tsx
@@ -1,25 +1,25 @@
 import './style.less'
 
 
-function About () {
+function About() {
 
     return (
         <>
             <section className='about-page'>
-                <ul>
-                    <li>
-                        1
-                    </li>
-                    <li>
-                        1
-                    </li>
-                    <li>
-                        1
-                    </li>
-                    <li>
-                        1
-                    </li>
-                </ul>
+                <h6 className='about-page-title'>Who I am ...</h6>
+
+                <div className="about-page-content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rutrum urna sit amet tortor commodo placerat. Aenean venenatis urna gravida est dictum, quis dictum urna gravida. Fusce quam nisl, gravida sed dui vel, ullamcorper volutpat arcu. Nam rutrum eros quis nibh vulputate, nec efficitur enim iaculis. Quisque eros velit, suscipit ut consequat quis, molestie at sem. Praesent sem ex, accumsan vitae bibendum ut, tristique at arcu. Vestibulum ornare, lorem eu rhoncus posuere, ipsum orci condimentum risus, sit amet congue lorem ante eu lorem. Mauris tempus venenatis felis, vitae convallis arcu rutrum vel. Mauris nec posuere ligula, ut maximus est. Quisque dapibus massa leo, id ornare turpis bibendum vitae. Fusce non pretium lorem. Nullam consequat ligula quis molestie tincidunt.
+
+                    Proin vitae leo dui. Nam et nisi quis erat semper porttitor at et felis. Sed cursus purus sapien, in consequat mauris hendrerit eget. Vestibulum id mollis libero. Mauris malesuada vel ante non mattis. Pellentesque justo libero, rutrum at placerat et, fringilla sit amet eros. Vestibulum feugiat efficitur ligula. Morbi eu velit leo. Cras risus dui, ornare in hendrerit nec, vulputate in justo. Curabitur nec blandit mi. Vestibulum condimentum tincidunt ex, in ullamcorper nulla rhoncus a. Pellentesque in urna at diam malesuada consectetur nec convallis metus. Vivamus vitae ultricies ex. Nulla ligula nisl, vestibulum vel commodo id, faucibus ut nisl. Nunc molestie velit dolor, ut tempus dolor faucibus et. Vivamus sit amet venenatis sem, ut tristique orci.
+
+                    Nam ultricies mattis congue. Sed dictum urna ac elit consectetur, vel volutpat massa malesuada. Aenean sit amet ipsum condimentum, ornare magna sed, eleifend orci. Cras lacinia ex dui, eu iaculis nisl iaculis et. Morbi placerat pharetra justo non condimentum. Donec mattis enim erat, ac elementum nisi aliquet nec. Pellentesque id arcu maximus, convallis justo pellentesque, condimentum risus. Nunc viverra ante ut est rhoncus, ac efficitur dui fringilla. Duis porttitor nisl id commodo auctor. Nulla quis lacinia neque. Curabitur sed augue arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum vestibulum sem quis ex finibus, at tempus quam commodo.
+
+                    Vestibulum eget ipsum elementum, porta sem non, sollicitudin tellus. Phasellus auctor euismod nisl eget ullamcorper. Etiam vel risus sit amet risus mollis porttitor et quis magna. Maecenas vestibulum eget mauris eget tempus. Mauris ac finibus lacus. Sed eget erat et sem maximus elementum. Nunc molestie diam fermentum mattis gravida. Donec convallis at ex condimentum semper. Fusce pulvinar, est nec mattis eleifend, sapien ligula porttitor libero, sed pulvinar est orci vitae velit. Etiam tincidunt lacus sem, ac ullamcorper lectus varius ac. Integer ut ullamcorper nunc, at rutrum lorem. Nulla facilisi. Nullam placerat massa justo, mollis porta mi interdum eget. Proin tempus lobortis diam vitae malesuada. Aliquam accumsan odio sed velit dapibus, vel iaculis purus laoreet.
+
+                    Etiam malesuada ex at porttitor porttitor. Aenean iaculis venenatis erat et volutpat. Vivamus dapibus purus arcu, non blandit magna blandit ac. Nunc elementum dapibus dolor. Pellentesque non iaculis tellus. Maecenas varius condimentum nunc, nec auctor arcu vehicula sed. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam mauris nisi, consectetur at nisl ut, molestie elementum diam. Morbi at magna vitae tortor venenatis varius. Mauris finibus diam sed fringilla lobortis.
+                </div>
+
             </section>
         </>
     )
