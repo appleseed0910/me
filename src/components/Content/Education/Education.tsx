9333dabd1a5f609d10cebcfafd0e00dd114fcf2d
appleseed0910 <xinyi.hu01@sap.com>
2026-04-03 18:56:00 +0800

    done all page templates

--- src/components/Content/Education/Education.tsx
+++ src/components/Content/Education/Education.tsx
@@ -1,10 +1,26 @@
-
+import './style.less'
 
 function Education() {
 
     return (
         <>
-            <div>Education</div>
+            <section className='edu-page'>
+                <h6 className='edu-page-title'>Years shaped me, </h6>
+
+                <div className="edu-page-content">
+                    <ul>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                    </ul>
+                </div>
+
+            </section>
         </>
     )
 }
--- src/components/Content/Experience/Experience.tsx
+++ src/components/Content/Experience/Experience.tsx
@@ -1,9 +1,26 @@
+import './style.less'
 
 function Experience () {
 
     return (
         <>
-            <div>Experience</div>
+            <section className='exp-page'>
+                <h6 className='exp-page-title'>Where I've been? </h6>
+
+                <div className="exp-page-content">
+                    <ul>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                        <li>1</li>
+                    </ul>
+                </div>
+
+            </section>
         </>
     )
 }
