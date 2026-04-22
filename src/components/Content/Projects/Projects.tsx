e8736179618cd1a363aa8a3735781947c1fab032
appleseed0910 <xinyi.hu01@sap.com>
2026-04-22 16:08:17 +0800

    add projects structure and intro txt

--- src/components/Content/Projects/Projects.tsx
+++ src/components/Content/Projects/Projects.tsx
@@ -10,18 +10,16 @@ function Projects() {
                 <h6 className='projects-page-title'>What I've done! </h6>
 
                 <div className="projects-page-content">
-
-                    <span className='highlight projects'>TEST</span>
-                    <ul>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                        <li>1</li>
-                    </ul>
+                    {/* <span className='highlight'>TEST</span> */}
+                    <p className='intro'>
+                        I have listed a few of my <span className='highlight projects'>personal</span> projects below,<br />for my <span className='highlight projects'>professional</span> projects plz refer to my resume :p
+                    </p>
+                    <div className='container'>
+                        <article className='chart'>1</article>
+                        <article className='quickgit'>2</article>
+                        <article className='autoexcel'>3</article>
+                        <article className='okr'>4</article>
+                    </div>
                 </div>
 
             </section>
