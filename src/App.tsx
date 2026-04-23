51c88ccc420204703f1bb297f6224cd8f5dc35b6
appleseed0910 <xinyi.hu01@sap.com>
2026-04-23 18:17:35 +0800

    add broswer router setup

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import Skills from './components/Content/Skills/Skills';
 
 function App() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename='/me/'>
           <Routes>
               <Route path="/" element={<Layout />}>
                   <Route index element={<About />} />
